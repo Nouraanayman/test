b698ad241052259d461e2d8c5bd872d04ac4b0b1
Nehal-Gamal11 <nehal.gamal2001@gmail.com>
2023-04-29 10:27:43 +0300

    .

--- orders.php
+++ orders.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="./login_css/ordercss.css"> 
     <meta charset="UTF-8">
@@ -9,7 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title> 
 </head>
-<body>
+<body style="background-image:url(./img/nordwood-themes-ivP3TYdLvw0-unsplash.jpg); background-repeat:no-repeat; background-position: top center; background-size:cover;" >
+<button class="back" >
+  <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
+  <span>Back</span>
+</button>
+<section class="curve">
   <h1>information</h1>
   <form class="orders">
 <div class="form__group field">
@@ -22,7 +26,7 @@
     <label for="name" class="form__label">email</label>
     
 </div>
-<div class="form__group field">
+<section class="form__group field">
     <input type="input" class="form__field" placeholder="phone number" required="">
     <label for="name" class="form__label">phone number</label>
     <button class="cta">
@@ -51,9 +55,14 @@ payments
 credit_card
 </span>    <span class="text-2">credit card</span>
   </span>
-  
 </a>
+<br>
+<button class="cta">
+  <span>place order</span>
+  <svg viewBox="0 0 13 10" height="10px" width="15px">
+  </svg>
+</button>
 </form>
-   
+</section>
 </body>
 </html>
